07c075b5e029e7305e01832c0d704e9efdb5b6a9
Calvin VanWormer <cvanworm@uwyo.edu>
2024-02-07 11:17:45 -0700

    refactor gen_hadimards function

--- Client/Shared/transform/matrix_gen.cs
+++ Client/Shared/transform/matrix_gen.cs
@@ -21,95 +21,83 @@ namespace HadimardGen {
             set;
         }
     }
-
-    // public class Matrices{
-    //     public IList<Matrix>? Hadamard {get; set;} = new List<Matrix>();
-    //     public int number_of_matrices {get; set;} = 0;
-       
-    // }
     
     //Class to generate and display hadimard matrices
     class Generator { 
         //Function to generate hadimard matrices up to HM
-        public static void generate_hadimard(int[,] previous_matrix, int start, int M){
-            Matrix matrix = new();
+        public static void gen_hadimards(int M){
+            int [,] previous_matrix = new int[2,2] {{1, 1},{1,-1}};
 
-            if(M == 1){
-                var walsh = generate_walsh(previous_matrix);
+            for(int i = 1; i <= M; i++){
+                var name = "H" + i;
+                var temp = next_matrix(previous_matrix, i);
+
+                var walsh = generate_walsh(temp);
                 var valid_walsh = check_repeats(walsh);
                 if (valid_walsh){
                     Console.WriteLine("Invalid walsh"); 
                     return;
                 }
-                matrix.walsh = walsh;
-                matrix.name = "H" + 1;
-                matrix.matrix = previous_matrix;
-                var fileName = $"hadimard_{1}.json";
-                Console.WriteLine(fileName);
-                var jsonout= JsonConvert.SerializeObject(matrix);
-                File.WriteAllText(fileName, jsonout);
-                Console.WriteLine(jsonout);
-                Console.WriteLine();
 
-                return;
-            }else{
-                //Creates and displays each hadimard matrix up until HM
-                for(int i = start; i <= M; i++){
-                    int size = (int)Math.Pow(2, i);
-                    int[,] temp = new int[size, size];
+                create_json(temp, walsh, name);
 
-                    //Fills the top left quadrant of the matrix with the previous matrix
-                    for(int j = 0; j < size/2; j++){
-                        for(int k = 0; k < size/2; k++){
-                            temp[j,k] = previous_matrix[j,k];
-                        }
-                    }
-
-                    //Fills the top right quadrant of the matrix with the previous matrix
-                    for(int j = 0; j < size/2; j++){
-                        for(int k = size/2; k < size; k++){
-                            temp[j,k] = previous_matrix[j,k - size/2];
-                        }
-                    }
-
-                    //Fills the bottom left quadrant of the matrix with the previous matrix
-                    for(int j = size/2; j < size; j++){
-                        for(int k = 0; k < size/2; k++){
-                            temp[j,k] = previous_matrix[j - size/2,k];
-                        }
-                    }
-
-                    //Fills the bottom right quadrant of the matrix with the inverse of the previous matrix
-                    for(int j = size/2; j < size; j++){
-                        for(int k = size/2; k < size; k++){
-                            temp[j,k] = -1 * previous_matrix[j - size/2,k - size/2];
-                        }
-                    }
-
-                    var walsh = new int[size];
-                    walsh = generate_walsh(temp);
-                    var valid_walsh = check_repeats(walsh);
-                    if (valid_walsh){
-                        Console.WriteLine("Invalid walsh"); 
-                        return;
-                    }
-                    //Writes Matrix to a JSON file
-                    matrix.walsh = walsh;
-                    matrix.name = "H" + i;
-                    matrix.matrix = temp;
-                    var fileName = $"hadimard_{i}.json";
-                    Console.WriteLine(fileName);
-                    var jsonout= JsonConvert.SerializeObject(matrix);
-                    File.WriteAllText(fileName, jsonout);
-                    Console.WriteLine(jsonout);
-                    Console.WriteLine();
-
-                    previous_matrix = temp;
-
-                }
-
+                previous_matrix = temp;
             }
+
+
         }
+
+        public static int[,] next_matrix(int[,] previous_matrix, int index){
+            int size = (int)Math.Pow(2, index);
+            int[,] temp = new int[size, size];
+
+            //Fills the top left quadrant of the matrix with the previous matrix
+            for(int j = 0; j < size/2; j++){
+                for(int k = 0; k < size/2; k++){
+                    temp[j,k] = previous_matrix[j,k];
+                }
+            }
+
+            //Fills the top right quadrant of the matrix with the previous matrix
+            for(int j = 0; j < size/2; j++){
+                for(int k = size/2; k < size; k++){
+                    temp[j,k] = previous_matrix[j,k - size/2];
+                }
+            }
+
+            //Fills the bottom left quadrant of the matrix with the previous matrix
+            for(int j = size/2; j < size; j++){
+                for(int k = 0; k < size/2; k++){
+                    temp[j,k] = previous_matrix[j - size/2,k];
+                }
+            }
+
+            //Fills the bottom right quadrant of the matrix with the inverse of the previous matrix
+            for(int j = size/2; j < size; j++){
+                for(int k = size/2; k < size; k++){
+                    temp[j,k] = -1 * previous_matrix[j - size/2,k - size/2];
+                }
+            }
+
+            return temp;
+        }
+
+        public static void create_json(int[,] hadimard, int[] walsh, string name){
+            Matrix matrix = new();
+
+            matrix.walsh = walsh;
+            matrix.name = name;
+            matrix.matrix = hadimard;
+
+            var fileName = $"hadimard_{name}.json";
+            Console.WriteLine(fileName);
+            var jsonout= JsonConvert.SerializeObject(matrix);
+            File.WriteAllText(fileName, jsonout);
+            // Console.WriteLine(jsonout);
+            Console.WriteLine();
+            
+        }
+        
 
         public static int[] generate_walsh(int[,] matrix){
             int[] walsh = new int[matrix.GetLength(0)];
@@ -148,25 +136,13 @@ namespace HadimardGen {
             return false;
         }
 
-        public static void display_matrix(int[,] matrix, int size){
-            // Displaying the final hadamard matrix
-                for (int k = 0; k < size; k++) {
-                    for (int j = 0; j < size; j++) {
-                        Console.Write(matrix[k,j] + " ");
-                    }
-                    Console.WriteLine();
-                }
-        }
-
     }
 
     class Program{
          // Main Method 
         static public void Main() { 
-            int[,] base_hadamard = new int[2,2] {{1, 1},{1,-1}};
 
-                
-            Generator.generate_hadimard(base_hadamard,1,5);
+            Generator.gen_hadimards(14);
                 
         } 
     }
