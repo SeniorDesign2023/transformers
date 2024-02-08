ee874d011e97ec552e5b837f23f48d9a53b4033e
Chet Russell <chet_dylan@outlook.com>
2024-02-07 17:12:04 -0700

    Generated API stubs, fixed engine namespace.

---
+++ Client/Shared/API/API.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Client.Shared.HadamardGen;
+
+namespace Client.Shared.API
+{
+    public class API
+    {
+        // This function generates the inital Hadamard matrices
+        public void initialize()
+        {
+        }
+
+        // This function will eventually return a list of integers so we can graph them
+        public void transform(string path, int m)
+        {
+            
+        }
+    }
+}
