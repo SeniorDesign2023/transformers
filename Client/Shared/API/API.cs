e2e1c6a222dabc05cfc6db52ada2936a69a8b200
Chet Russell <chet_dylan@outlook.com>
2024-02-17 15:54:34 -0700

    Added forwardTransform function

--- Client/Shared/API/API.cs
+++ Client/Shared/API/API.cs
@@ -29,5 +29,35 @@ namespace Client.Shared.API
 
             return Transform.hadimard_transform(list, size);
         }
+
+        public int[] forwardTransform(int[] list, bool truncate, int percent)
+        {
+            int len = list.Length;
+            int size;
+            float percentage = percent / (float)100;
+            int[] tmp = new int[len];
+            //creating a deep copy
+            Array.Copy(list, tmp, len);
+
+            // checking if we are truncating or not
+            if (truncate)
+            {
+                size = (int)Floor(Log(len, 2));
+            }
+            else
+            {
+                size = (int)Ceiling(Log(len, 2));
+            }
+            // resizing the array to the calculated truncation
+            Array.Resize(ref tmp, size);
+
+            // actually doing transform
+            int[] ret = Transform.hadimard_transform(tmp, size);
+
+            // resizing based on percentage
+            Array.Resize(ref ret, (int)(size * (1 - percentage)));
+
+            return ret;
+        }
     }
 }
