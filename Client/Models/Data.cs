056667ece14e40220097f6d9ad261a109334fb4b
Finn Tomasula Martin <ftomasul@uwyo.edu>
2024-04-07 15:30:19 -0600

    Modified things

--- Client/Models/Data.cs
+++ Client/Models/Data.cs
@@ -3,6 +3,8 @@ namespace Client.Models
     public class Data
     {
         public double[] Elements { get; set; }
+        public bool Truncate { get; set; }
+        public bool Inverse { get; set;}
         public int Compression { get; set; }
     }
 }
